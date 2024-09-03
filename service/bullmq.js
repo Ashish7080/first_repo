d3044db136b0e8fc9a6aec955229368d07069a5c
Ram Ashish <aram97010@gmail.com>
2024-09-03 23:12:11 +0530

    added bullmq to process the job

---
+++ service/bullmq.js
@@ -0,0 +1,13 @@
+import { Queue } from "bullmq";
+
+
+const queue = new Queue("Paint",{
+    connection:{
+        host:"localhost",
+        port:6379
+    }
+});
+
+
+queue.add("cars", { color: "blue" });
+queue.add("planes", { quality: "super" });
